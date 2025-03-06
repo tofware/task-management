ecbcc3d2f19a6cbd9b6527d18fc795563b946391
Tofan <tofanalin1@gmail.com>
2025-03-06 18:35:37 +0200

    second commit

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
